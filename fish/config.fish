087979eee1a3ef3adacadc5505d84086f855eb67
retroVision <pupsikmoi2010@gmail.com>
2025-07-22 13:34:08 +0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,6 @@ if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
 end
 
 set -g fish_greeting
-set -gx PATH $HOME/.nvm/versions/node/v18.20.8/bin $PATH
 if status is-interactive
   	starship init fish | source
 end
