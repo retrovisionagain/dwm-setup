950537a90532bfd9c5c6c4da335ae6e83532ca77
retroVision <pupsikmoi2010@gmail.com>
2025-08-15 00:24:38 +0500

    Delete dwm directory

--- dwm/themes/new-theme.h
+++
@@ -1,5 +0,0 @@
-static const char col_gray1[] = "#000000";
-static const char col_gray2[] = "#000000";
-static const char col_gray3[] = "#cdd6f4";
-static const char col_gray4[] = "#000000";
-static const char col_accnt[] = "#6376cf";
